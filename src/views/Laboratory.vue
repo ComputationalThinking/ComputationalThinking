cf401bc2138724e3cafd48642f0a8d79fff3605b
ljjlxh <2868339870@qq.com>
2021-02-18 22:01:01 +0800

    [lab]detail

--- src/views/Laboratory.vue
+++ src/views/Laboratory.vue
@@ -1,11 +1,124 @@
 <template>
   <div class="laboratory">
-    <h1>this is 实验室 page</h1>
+    <div class="left">
+      <p class="class1">计算思维</p>
+      <p class="class2">实验室</p>
+      <p @click="click1" @mouseover="mouseOver1" @mouseleave="mouseLeave" :style="mem1" class="class3">实验室地点、图片</p>
+      <p @click="click2" @mouseover="mouseOver2" @mouseleave="mouseLeave" :style="mem2" class="class4">实验室研究方向、人员需求</p>
+      <p @click="click3" @mouseover="mouseOver3" @mouseleave="mouseLeave" :style="mem3" class="class4">实验室相关人员</p>
+    </div>
+    <div class="right">
+      <div :style="mem4"><img :src="pic2"></div>
+      <div class="picc" :style="mem5">
+        bb
+      </div>
+      <div class="picc" :style="mem6">
+        aa
+      </div>
+      <div class="picc" :style="mem7">
+        cc
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name:'laboratory',
+  data(){
+    return {
+      pic1:require("@/assets/images/ren1.png"),
+      pic2:require("@/assets/images/image1.png"),
+      pic3:require("@/assets/images/ren2.png"),
+      pic4:require("@/assets/images/女.jpg"),
+      mem1: "",
+      mem2: "",
+      mem3: "",
+      mem4: "",
+      mem5: "",
+      mem6: "",
+      mem7: "",
+    }
+  },
+  methods:{
+    mouseOver1() {
+      this.mem1 = "color:black";
+    },
+    mouseOver2() {
+      this.mem2 = "color:black";
+    },
+    mouseOver3() {
+      this.mem3 = "color:black";
+    },
+    mouseLeave() {
+      this.mem1 = "";
+      this.mem2 = "";
+      this.mem3 = "";
+    },
+    click1() {
+      this.mem4 = "display: none";
+      this.mem5 = "display: block";
+      this.mem6 = "display: none";
+      this.mem7 = "display: none";
+    },
+    click2() {
+      this.mem4 = "display: none";
+      this.mem5 = "display: none";
+      this.mem6 = "visibility: visible";
+      this.mem7 = "display: none";
+    },
+    click3() {
+      this.mem4 = "display: none";
+      this.mem5 = "display: none";
+      this.mem6 = "display: none";
+      this.mem7 = "visibility: visible";
+    },
+  }
+}
 </script>
 
-<style></style>
+<style>
+.laboratory{
+  color: black;
+  height: 41.1em;
+  overflow: hidden;
+}
+.left{
+  width: 40%;
+  float: left;
+  height: 41.1em;
+}
+.right{
+  width: 60%;
+  float: left;
+  height: 41.1em;
+}
+.class1{
+  width: 100%;
+  font-size: 400%;
+  margin-left: 25%;
+  margin-top: 2em;
+}
+.class2{
+  font-size: 550%;
+  margin-left: 50%;
+  margin-top: 0%;
+  width: 100%;
+}
+.class3{
+  color: rgb(160, 159, 159);
+  font-size: 170%;
+  margin-left: 35%;
+  margin-top: 3em;
+}
+.class4{
+  color: rgb(160, 159, 159);
+  font-size: 170%;
+  margin-left: 35%;
+  margin-top: 1em;
+}
+.picc{
+  height: 41.1em;
+  margin-top: -41.1em;
+}
+</style>
---
+++ src/views/skip/DetailMes.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="detailmes">
+    <div class="shang">
+        <div class="con1">
+            <div class="kuang"></div>
+            <div class="photo">
+                <img :src="pic1" style="width:99%;height:99%;">
+            </div>
+        </div>
+        <div class="con2">
+            <p class="title">个人简介：</p><br><br><br>
+            <p>杨娟(Juan Yang)<br>Sichuan</p><br>
+            <p> Faculty of Psychology,<br>Southwest University,Chongqing, China<br>No.2 Tiansheng Road, Beibei,
+                Chongqing, China, 400715 </p><br>
+            <p>Tel: +86-23-68367697</p><br>
+            <p>E-mail: valleyqq@swu.edu.cn;yangjuan923@yahoo.com</p><br>
+        </div>
+    </div>
+    <div class="xia">
+        <p style="line-height:30px">
+            EMPLOYMENT & EDUCATION ——— Professor of Psychology, Social Psychology, 2014-present, Southwest University, China.
+            Visiting Scholar, 2013-2014, Department of Psychology, Peking University.
+            Associate professorof Psychology, Basic Psychology, 2011-2014, Southwest University, China.
+            Lecturer, Basic Psychology, 2009-2011, Southwest University, China.
+            Visiting Ph.D. student, 2008-2009, Douglas Hospital, McGill University, Canada.
+            Ph.D., Developmental and Educational Psychology, 2009, Southwest University, China.
+            B.A., Educational Psychology, 2004, Sichuan Normal University, China.
+        </p><br>
+        <p> AWARDS & HONORS </p><br>
+        <p>Excellent Doctoral thesis, Chongqing Educational commission, 2010</p>
+    </div>
+    <div class="white"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detailmes',
+  data() {
+    return {
+        pic1:require("@/assets/images/男.jpg"),
+    }
+  }
+}
+</script>
+
+<style>
+.shang{
+    height: 28.125em;
+    float: top;
+}
+.title{
+    font-size: 400%;
+    font-weight:500;
+    color:#494949;
+}
+.kuang{
+    margin-top: 10%;
+    width: 22em;
+    height: 22em;
+    background-color: white;
+    border: 10px solid #FFC78E;
+    max-width: 80%;
+}
+.photo {
+    max-width: 80%;
+    margin-top: -380px;
+    margin-left: 2em;
+    width: 22em;
+    height: 22em;
+    padding: 0.8em 0.5em 0.8em 0.8em;
+    border: 2px solid black;
+}
+.con1{
+    float: left;
+    width: 50%;
+    padding-left:20%;
+    padding-top: 2%;
+}
+.con2 {
+    width: 40%;
+    float: left;
+    margin-top: 6%;
+    margin-bottom: 2%;
+    margin-left: 2em;
+}
+.xia {
+    margin-left: 18%;
+    float: top;
+    width:63%;
+    min-width: 800px;
+}
+</style>
