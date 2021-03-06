ba3df1f8293969d1169e8614b1180046b9823ec0
Dai303 <1471324635@qq.com>
2021-03-06 14:54:21 +0800

    [main]detail

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import './plugins/element.js'
 import '../font-awesome/css/font-awesome.min.css'
 
