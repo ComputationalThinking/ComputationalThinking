a640a7d240e1d11e9b84300cd24dd0fe82be528e
Dai303 <1471324635@qq.com>
2021-03-11 21:06:51 +0800

    [axios]detail

--- src/views/message_content/information.vue
+++ src/views/message_content/information.vue
@@ -345,7 +345,7 @@
               @size-change="handleSizeChange"
               @current-change="handleCurrentChange"
               :current-page.sync="currentPage"
-              :page-size="100"
+              :page-size="11"
               :hide-on-single-page="true"
               layout="prev, pager, next, jumper"
               :total="1000"
@@ -418,7 +418,7 @@ export default {
   created() {
     const that = this
     this.$axios
-      .get('http://localhost:8081/News/information')
+      .get('http://localhost:8083/News/information')
       .then(function(response) {
         // that.message = response.data
         that.message = response.data
